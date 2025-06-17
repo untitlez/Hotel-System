d35db803180c2b99c4aaa8ebc268efc8c05def67
untitlez <tleza_championship2@hotmail.com>
2025-06-17 17:23:21 +0700

    add: api route

---
+++ app/api/rooms/[id]/route.ts
@@ -0,0 +1,60 @@
+import { prisma } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const payload = await prisma.room.findUnique({
+      where: {
+        id: params.id,
+      },
+    });
+    return NextResponse.json(payload);
+  } catch (error) {
+    console.error("error", error);
+    return NextResponse.json({ message: "Something went wrong" });
+  }
+}
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const body = await req.json();
+    const payload = await prisma.room.update({
+      where: {
+        id: params.id,
+      },
+      data: body,
+    });
+    return NextResponse.json({
+      message: "Update successfully",
+      data: payload,
+    });
+  } catch (error) {
+    console.error("error", error);
+    return NextResponse.json({ message: "Something went wrong" });
+  }
+}
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    await prisma.room.delete({
+      where: {
+        id: params.id,
+      },
+    });
+    return NextResponse.json({
+      message: "Delete successfully",
+    });
+  } catch (error) {
+    console.error("error", error);
+    return NextResponse.json({ message: "Something went wrong" });
+  }
+}
